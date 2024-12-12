6268acb4cd1bfb6877cfca21405c5acbe03ba6a9
We8p4gE <156445954+We8p4gE@users.noreply.github.com>
2024-12-12 18:35:41 +0900

    Update library.js

--- library.js
+++ library.js
@@ -1,2 +1,14 @@
-
-console.log('test')
+function unix_decode(timestamp, format) {
+    const date = new Date(timestamp * 1000);
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    const D = date.getDate();
+    const M = date.getMonth() + 1;
+    const Y = date.getFullYear();
+    if (format) {
+        return eval('`' + format + '`')
+    } else {
+        return `${h}:${m}, ${D}/${M}/${Y}`
+    }
+}
