f941b9483f2da065bd535410e587a6ba0a720870
We8p4gE <156445954+We8p4gE@users.noreply.github.com>
2024-12-12 18:51:18 +0900

    Update library.js

--- library.js
+++ library.js
@@ -1,14 +1,8 @@
 function unix_decode(timestamp, format) {
-    const d = new Date(timestamp * 1000);
-    const h = d.getHours();
-    const m = d.getMinutes();
-    const s = d.getSeconds();
-    const D = d.getDate();
-    const M = d.getMonth() + 1;
-    const Y = d.getFullYear();
-    if (format) {
-        return eval('`' + format + '`')
-    } else {
-        return `${h}:${m}, ${D}/${M}/${Y}`
-    }
+    const d = new Date(timestamp * 1000),
+    h = d.getHours(), m = d.getMinutes(),
+    s = d.getSeconds(), D = d.getDate(),
+    M = d.getMonth() + 1, Y = d.getFullYear();
+    return format ? eval('`' + format + '`')
+    : `${h}:${m}:${s}, ${D}/${M}/${Y}`;
 }
